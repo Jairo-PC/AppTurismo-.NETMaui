16b8ac00ef00b9dcb96ce6a2415bcd30053b42f7
Jairo-PC <123668353+Jairo-PC@users.noreply.github.com>
2023-11-24 13:39:19 -0500

    Cambio turismo

--- js/slide.js
+++ js/slide.js
@@ -34,4 +34,34 @@ var swiper = new Swiper(".slide-container", {
   },
 });
 
+var swiper = new Swiper(".slide-container-dato", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2, // Cambiado a 2 para mostrar solo dos ejemplos
+    },
+    // Puedes eliminar los breakpoints adicionales que no necesitas
+  },
+});
+
+
 
